197f56bdda8b55b74b08cd9df5a95a0ec6b36f48
bitsandcels <33361851+bitsandcels@users.noreply.github.com>
2018-04-08 17:32:47 -0700

    Adjusted main and added file

--- Graphics_Core/main.cpp
+++ Graphics_Core/main.cpp
@@ -12,7 +12,7 @@ int main()
 
 	sf::Font font;
 
-	if (!font.loadFromFile("sansation.ttf"))
+	if (!font.loadFromFile("arial.ttf"))
 	{
 		std::cout << "FAILED LOADING FILE" << std::endl;
 	}
@@ -20,25 +20,25 @@ int main()
 	sf::Text text[6];
 
 	text[0].setFont(font);
-	text[0].setString("Start Game");
-	text[0].setPosition(325.0f, 125.0f);
+	text[0].setString("Change to Red");
+	text[0].setPosition(400.0f, 190.0f);
 
 	text[1].setFont(font);
-	text[1].setString("Quit");
-	text[1].setPosition(375.0f, 325.0f);
+	text[1].setString("Change to Blue");
+	text[1].setPosition(400.0f, 350.0f);
 
-	//sf::Event event;
-	//sf::CircleShape circleShape(200);
-	//circleShape.setFillColor(sf::Color::Blue);
+	text[2].setFont(font);
+	text[2].setString("It Disappeared!");
+	text[2].setPosition(15.0f, 425.0f);
 
 	graphicsCore.SetGraphics(0.0f, 0.0f, 200, 4, sf::Color::Magenta, 1, false, 7);
-	graphicsCore.SetGraphics(0.0f, 0.0f, 100, 3, sf::Color::Red, 0, false, 8);
+	graphicsCore.SetGraphics(0.0f, 400.0f, 100, 3, sf::Color::White, 0, false, 8);
 
 	const int numButtons = 2;
 	Button buttons[numButtons];
 
-	buttons[0] = *new Button(400.0f, 25.0f, 100, graphicsCore, 1, sf::Color::Red);
-	buttons[1] = *new Button(400.0f, 200.0f, 100, graphicsCore, 2, sf::Color::Blue);
+	buttons[0] = *new Button(400.0f, 115.0f, 100, graphicsCore, 1, sf::Color::Red);
+	buttons[1] = *new Button(400.0f, 275.0f, 100, graphicsCore, 2, sf::Color::Blue);
 
 
 	int loopCount = 0;
@@ -64,6 +64,12 @@ int main()
 		loopState = graphicsCore.Update(renderWindow, false);
 		renderWindow.draw(text[0]); //render text above button
 		renderWindow.draw(text[1]); //render text above button
+
+		if (loopCount > 1800 && loopCount < 2700)
+		{
+			renderWindow.draw(text[2]);
+		}
+
 		renderWindow.display();
 		renderWindow.clear(sf::Color::Black);
 	}
